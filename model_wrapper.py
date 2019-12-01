7683c020ceffe644690c6f9e36fe9450016d83e9
ekremozturk <ozturk@informatik.uni-freiburg.de>
2019-12-01 15:09:03 +0100

    bug fixes

--- model_wrapper.py
+++ model_wrapper.py
@@ -6,6 +6,7 @@ Created on Thu Nov 28 13:22:22 2019
 @author: ekrem
 """
 import time
+import pickle
 
 import pandas as pd
 import numpy as np
@@ -33,6 +34,7 @@ class Wrapper():
         self.participants_ID = np.unique(data.participant_code.values)
         self.num_sets = num_sets
         self.models = [launch_mreasoner_model() for i in range(num_sets)]
+        self.optim_params = [m.params for m in self.models]
         
         
     def canonical_train_test_split(self):
@@ -59,19 +61,18 @@ class Wrapper():
         capacity = dict()
         
         for ID in self.participants_ID:
-            capacity[ID] = self.data[self.data.participant_code == ID].feature
+            capacity[ID] = self.data[self.data.participant_code == ID].iloc[0][feature]
         
         self.feature = feature
         self.capacity = capacity
         
     
     def uniform_criterion(self):
-        if self.criterion:
+        if self.capacity:
             scores = list(self.capacity.values())
         else:
             print('Capacity dictionary is missing. Stopping!')
             return
-        
         min_score, max_score = min(scores), max(scores)
         uniform_range = (max_score-min_score)/self.num_sets
         
@@ -83,11 +84,11 @@ class Wrapper():
         
         self.criterion = criterion
         
-        self.data.wm_capacity = self.data.apply(self.uniform_split, axis = 1)
+        self.data.wm_capacity = self.data.apply(self.split, axis = 1)
     
     
     def gaussian_criterion(self):
-        if self.criterion:
+        if self.capacity:
             scores = list(self.capacity.values())
         else:
             print('Capacity dictionary is missing. Stopping!')
@@ -114,23 +115,35 @@ class Wrapper():
             if score >= min_cap and score <= max_cap:
                 return i
     
-    
-    def canonical_fit(self, epochs):
-        model = self.model[0]
-        print('Model fit is starting...')
+    def canonical_random_search(self, epochs):
+        model = self.models[0]
+        print('Random search is starting...')
         start = time.time()
-        train_score, optim_param_set = model.fit(self.train_data.syllog.values, 
-                                                 self.train_data.syllog_conclusion.values, 
-                                                 num_fits = epochs)
+        error, optim_param_set = model.fit_rnd(self.train_data.syllog.values, 
+                                               self.train_data.syllog_conclusion.values, 
+                                               num = epochs)
         end = time.time()
         print('Time taken to fit %f' %(end-start))
         
-        test_score = 1-model._fit_fun(optim_param_set, 
+        self.models = [model]
+        self.optim_params = [optim_param_set]
+        self.search_scores = [1-error]
+        
+    def canonical_fit(self, epochs):
+        model = self.models[0]
+        print('Model fit is starting...')
+        start = time.time()
+        train_score, _ = model.fit(self.train_data.syllog.values, 
+                                   self.train_data.syllog_conclusion.values, 
+                                   num_fits = epochs)
+        end = time.time()
+        print('Time taken to fit %f' %(end-start))
+        
+        test_score = 1-model._fit_fun(self.optim_params[0], 
                                       self.test_data.syllog.values, 
                                       self.test_data.syllog_conclusion.values)
         
         self.models = [model]
-        self.optim_params = [optim_param_set]
         self.train_scores = [train_score]
         self.test_scores = [test_score]
         
@@ -140,19 +153,19 @@ class Wrapper():
         optim_params = []
         train_scores = []
         test_scores = []
-        for model, cluster in zip(self.model, self.clustered_data):
+        for model, cluster in zip(self.models, self.clustered_data):
             print('Model fit is starting...')
             train_data, test_data = cluster
             start = time.time()
-            train_score, optim_param_set = model.fit(self.train_data.syllog.values, 
-                                                     self.train_data.syllog_conclusion.values, 
+            train_score, optim_param_set = model.fit(train_data.syllog.values, 
+                                                     train_data.syllog_conclusion.values, 
                                                      num_fits = epochs)
             end = time.time()
             print('Time taken to fit %f' %(end-start))
             
             test_score = 1-model._fit_fun(optim_param_set, 
-                                          self.test_data.syllog.values, 
-                                          self.test_data.syllog_conclusion.values)
+                                          test_data.syllog.values, 
+                                          test_data.syllog_conclusion.values)
             
             models.append(model)
             optim_params.append(optim_param_set)
@@ -160,20 +173,55 @@ class Wrapper():
             test_scores.append(test_score)
         
         self.models = models
-        self.optim_params = optim_params
         self.train_scores = train_scores
         self.test_scores = test_scores
             
 # Read data
 initial_data = pd.read_csv('../wm_syllog_data.csv', sep = ';', header = 0)
 
-# Create and fit canonical model
-w_benchmark = Wrapper(initial_data, 1)
+# TODO: Create and fit benchmark model to all data
+w_benchmark = Wrapper(initial_data, num_sets = 1)
 w_benchmark.canonical_train_test_split()
-w_benchmark.canonical_fit(epochs = 1)
+w_benchmark.canonical_random_search(epochs = 10)
+w_benchmark.canonical_fit(epochs = 3)
 
+with open('benchmark.model', 'w+') as f:
+    pickle.dump(w_benchmark, f)
+
+
+'''
+# TODO: Create and fit models to uniform split data
+w_uniform = Wrapper(initial_data, num_sets = 3)
+w_uniform.set_capacity('os_max_span')
+w_uniform.uniform_criterion()
+w_uniform.clustered_train_test_split()
+w_uniform.clustered_fit(epochs = 3)
+
+# TODO: Create and fit models to gaussian split data
+w_gaussian = Wrapper(initial_data, num_sets = 3)
+w_gaussian.set_capacity('os_max_span')
+w_gaussian.gaussian_criterion()
+w_gaussian.clustered_train_test_split()
+w_gaussian.clustered_fit(epochs = 3)
+'''
+
+'''
 train_score = w_benchmark.train_scores[0]
 test_score = w_benchmark.test_scores[0]
+
+optim_params = w_benchmark.optim_params
+
+train_scores = w_uniform.train_scores
+test_scores = w_uniform.test_scores
+
+optim_params = w_uniform.optim_params
+
+train_scores = w_gaussian.train_scores
+test_scores = w_gaussian.test_scores
+
+optim_params = w_gaussian.optim_params
+'''
+
 
 '''
 preds = []
@@ -187,8 +235,3 @@ for pred, truth in zip(preds, test_data.syllog_conclusion.values):
     
 my_test = sum(individual_scores)/len(individual_scores)
 '''
-
-# TODO: Create and fit models to uniform split data
-
-# TODO: Create and fit models to gaussian split data
-
